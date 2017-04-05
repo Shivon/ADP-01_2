8e2edf4c2c09c8704b0fc8c2f79f543ccee9f04c
Shivon <marjan.bachtiari@haw-hamburg.de>
2017-04-05 16:00:57 +0200

    Update list.erl as per coding convention

--- list.erl
+++ list.erl
@@ -1,6 +1,5 @@
 -module(list).
--compile({no_auto_import,[length/1]}).
--export([create/0, isEmpty/1, length/1, isList/1, equal/2, find/2, retrieve/2, delete/2, insert/3, concat/2, diffList/2, eoCount/1]).
+-export([create/0, is_empty/1, len/1, is_list/1, equal/2, find/2, retrieve/2, delete/2, insert/3, concat/2, diff_list/2, even_odd_count/1]).
 
 % List model: {1, {2, {3, {}}}} => Pos1 = 1, Pos2 = 2 etc
 % test lists:
@@ -15,22 +14,22 @@
 create() -> {}.
 
 
-% isEmpty: list → bool
-% isEmpty(List) -> List == {}.
-isEmpty(List) -> length(List) == 0.
+% is_empty: list → bool
+% is_empty(List) -> List == {}.
+is_empty(List) -> len(List) == 0.
 
 
-% length: list → int
-length(List) -> length_(List, 0).
+% len: list → int
+len(List) -> len(List, 0).
 
-length_({}, Accu) -> Accu;
-length_({_Head, Tail}, Accu) -> length_(Tail, Accu + 1).
+len({}, Accu) -> Accu;
+len({_Head, Tail}, Accu) -> len(Tail, Accu + 1).
 
 
-% isList: list → bool
-isList({}) -> true;
-isList({_Head, Tail}) -> isList(Tail);
-isList(_) -> false.
+% is_list: list → bool
+is_list({}) -> true;
+is_list({_Head, Tail}) -> is_list(Tail);
+is_list(_) -> false.
 
 
 % equal: list × list → bool
@@ -43,11 +42,11 @@ equal(_, _) -> false.
 
 % find: list × elem → pos
 % returns nil when element is not found
-find(List, Element) -> find_(List, Element, 1).
+find(List, Element) -> find(List, Element, 1).
 
-find_({}, _, _) -> nil;
-find_({Head, _Tail}, Element, AccuPosition) when Head == Element -> AccuPosition;
-find_({_Head, Tail}, Element, AccuPosition) -> find_(Tail, Element, AccuPosition + 1).
+find({}, _, _) -> nil;
+find({Head, _Tail}, Element, AccuPosition) when Head == Element -> AccuPosition;
+find({_Head, Tail}, Element, AccuPosition) -> find(Tail, Element, AccuPosition + 1).
 
 
 % retrieve: list × pos → elem
@@ -78,62 +77,62 @@ insert({}, _, _) -> {}.
 concat({}, List) -> List;
 concat(List, {}) -> List;
 concat(FirstList, {Head, Tail}) ->
-  LengthFirstList = length(FirstList),
+  LengthFirstList = len(FirstList),
   AccuList = insert(FirstList, LengthFirstList + 1, Head),
   concat(AccuList, Tail).
 
 
-% diffListe: list × list → list
-diffList(FirstList, SecondList) -> diffList_(FirstList, SecondList, {}).
+% diff_liste: list × list → list
+diff_list(FirstList, SecondList) -> diff_list(FirstList, SecondList, {}).
 
-diffList_(List, {}, ResultList) -> concat(List, ResultList);
-diffList_({}, List, ResultList) -> concat(List, ResultList);
-diffList_(FirstList, SecondList, ResultList) ->
+diff_list(List, {}, ResultList) -> concat(List, ResultList);
+diff_list({}, List, ResultList) -> concat(List, ResultList);
+diff_list(FirstList, SecondList, ResultList) ->
   {FirstHead, FirstTail} = FirstList,
   {SecondHead, SecondTail} = SecondList,
-  FirstHeadInSecondList = elementInList(FirstHead, SecondList),
-  SecondHeadInFirstList = elementInList(SecondHead, FirstList),
+  FirstHeadInSecondList = element_in_list(FirstHead, SecondList),
+  SecondHeadInFirstList = element_in_list(SecondHead, FirstList),
 
   if
     FirstHeadInSecondList and SecondHeadInFirstList ->
-      NewFirstList = deleteAll(FirstTail, SecondHead),
-      NewSecondList = deleteAll(SecondTail, FirstHead),
-      diffList_(NewFirstList, NewSecondList, ResultList);
+      NewFirstList = delete_all(FirstTail, SecondHead),
+      NewSecondList = delete_all(SecondTail, FirstHead),
+      diff_list(NewFirstList, NewSecondList, ResultList);
 
     FirstHeadInSecondList ->
-      NewSecondList = deleteAll(SecondTail, FirstHead),
-      diffList_(FirstTail, NewSecondList, {SecondHead, ResultList});
+      NewSecondList = delete_all(SecondTail, FirstHead),
+      diff_list(FirstTail, NewSecondList, {SecondHead, ResultList});
 
     SecondHeadInFirstList ->
-      NewFirstList = deleteAll(FirstTail, SecondHead),
-      diffList_(NewFirstList, SecondTail, {FirstHead, ResultList});
+      NewFirstList = delete_all(FirstTail, SecondHead),
+      diff_list(NewFirstList, SecondTail, {FirstHead, ResultList});
 
-    true -> diffList_(FirstTail, SecondTail, {FirstHead, {SecondHead, ResultList}})
+    true -> diff_list(FirstTail, SecondTail, {FirstHead, {SecondHead, ResultList}})
   end.
 
 
-% eoCount: list → [int,int]
-% eoCount(L) counts the lists with even and odd length within list L including list L itself. An empty list is considered to have
+% even_odd_count: list → [int,int]
+% even_odd_count(L) counts the lists with even and odd length within list L including list L itself. An empty list is considered to have
 % even length. List L can include elements which are no lists. Return value is the tuple [<number or even lists>, <number of odd lists>]
 % Note: the empty list, which indicates the end of the list, is not counted as element or as even list
-eoCount(List) ->
-  IsEven = hasEvenLength(List),
+even_odd_count(List) ->
+  IsEven = has_even_length(List),
   if
-    IsEven -> eoCount_(List, [1, 0]);
-    not(IsEven) -> eoCount_(List, [0, 1])
+    IsEven -> even_odd_count(List, [1, 0]);
+    not(IsEven) -> even_odd_count(List, [0, 1])
   end.
 
-eoCount_({}, Result) -> Result;
-eoCount_(List, [Even, Odd]) ->
+even_odd_count({}, Result) -> Result;
+even_odd_count(List, [Even, Odd]) ->
   {Head, Tail} = List,
-  HeadIsList = isList(Head),
+  HeadIsList = is_list(Head),
   if
-    not(HeadIsList) -> eoCount_(Tail, [Even, Odd]);
+    not(HeadIsList) -> even_odd_count(Tail, [Even, Odd]);
     HeadIsList ->
-      HeadIsEven = hasEvenLength(Head),
+      HeadIsEven = has_even_length(Head),
       if
-        HeadIsEven -> eoCount_(Tail, [Even + 1, Odd]);
-        not(HeadIsEven) -> eoCount_(Tail, [Even, Odd + 1])
+        HeadIsEven -> even_odd_count(Tail, [Even + 1, Odd]);
+        not(HeadIsEven) -> even_odd_count(Tail, [Even, Odd + 1])
       end
   end.
 % {{1, {2, {}}}, {{1, {2, {}}}, {{101, {1, {2, {}}}}, {1, {2, {}}}}}}.
@@ -141,22 +140,22 @@ eoCount_(List, [Even, Odd]) ->
 
 
 % Helper
-% elementInList: elem × list → bool
-elementInList(_Element, {}) -> false;
-elementInList(Element, {Head, _Tail}) when Element == Head -> true;
-elementInList(Element, {_Head, Tail}) -> elementInList(Element, Tail).
+% element_in_list: elem × list → bool
+element_in_list(_Element, {}) -> false;
+element_in_list(Element, {Head, _Tail}) when Element == Head -> true;
+element_in_list(Element, {_Head, Tail}) -> element_in_list(Element, Tail).
 
 
-% deleteAll: list × elem → list
+% delete_all: list × elem → list
 % deletes all elements with the same value as given element from a given list and returns new list
-deleteAll({}, _) -> {};
-deleteAll({Head, Tail}, Element) when Head == Element -> deleteAll(Tail, Element);
-deleteAll({Head, Tail}, Element) -> {Head, deleteAll(Tail, Element)}.
+delete_all({}, _) -> {};
+delete_all({Head, Tail}, Element) when Head == Element -> delete_all(Tail, Element);
+delete_all({Head, Tail}, Element) -> {Head, delete_all(Tail, Element)}.
 
 
-% hasEvenLength: list → bool
-hasEvenLength(List) -> isEven(length(List)).
+% has_even_length: list → bool
+has_even_length(List) -> is_even(len(List)).
 
 
-% isEven: integer → bool
-isEven(Integer) -> Integer rem 2 == 0.
+% is_even: integer → bool
+is_even(Integer) -> Integer rem 2 == 0.
