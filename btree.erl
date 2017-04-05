f3c2e8a33ad8bdb9f3b163fdd52c9bec4aa3ca12
Shivon <marjan.bachtiari@haw-hamburg.de>
2017-04-05 15:51:50 +0200

    Update btree.erl as per coding convention

--- btree.erl
+++ btree.erl
@@ -1,84 +1,84 @@
 % Technisch (nach innen)
 %
-%     1. equalBT testet auf strukturelle Gleichheit.
-%     3. isBT überprüft, ob BTree von korrekter syntaktischer Struktur ist und ob die Semantik (Sortierung und Höhe) korrekt ist.
+%     1. equal testet auf strukturelle Gleichheit.
+%     3. is_btree überprüft, ob BTree von korrekter syntaktischer Struktur ist und ob die Semantik (Sortierung und Höhe) korrekt ist.
 %         Bei der Prüfung darf der Baum nur einmal von oben nach unten durchlaufen werden und wegen der Rekursion dann einmal von unten nach oben.
 %         Bei der semantischen Prüfung eines Knotens haben also alle dazu benötigten Informationen direkt verwertbar als Datenstruktur vorzuliegen.
 %
 % Objektmengen:  elem, btree, high
 % Operationen: (semantische Signatur) / (syntaktische Struktur)
-% isBT: btree → bool                                    / isBT(<BTree>)
-% isEmptyBT: btree → bool                         / isEmptyBT(<BTree>)
-% equalBT: btree × btree → bool                / equalBT(<BTree>,<BTree>)
+% is_btree: btree → bool                                    / is_btree(<BTree>)
+% is_empty: btree → bool                         / is_empty(<BTree>)
+% equal: btree × btree → bool                / equal(<BTree>,<BTree>)
 
 -module(btree).
 -compile(export_all).
-% -export([initBT/0, isBT/1, insertBT/2, isEmptyBT/1, equalBT/2]).
+% -export([init_btree/0, is_btree/1, insert_node/2, is_empty/1, equal/2]).
 
 
-% initBT: ∅ → btree
-initBT() -> {}.
+% init_btree: ∅ → btree
+init_btree() -> {}.
 
 % Pattern {{Parent, Height}, {LeftChild, Height}, {RightChild, Height}}
 % Height of the tree: Empty tree = 0, only root = 1 and so on
 % Attention: Height != Level! Height at each node is the height of the partial tree from this node on,
 % so the height gets smaller towards the leafs
 
-% isBT: btree → bool
-% isBT(<BTree>)
+% is_btree: btree → bool
+% is_btree(<BTree>)
 
 
-% insertBT: btree × elem → btree
+% insert_node: btree × elem → btree
 % elements in the partial left tree are smaller and in the partial right tree greater than parent
 % elements == parent are ignored
-insertBT({}, Element) -> initLeaf(Element);
+insert_node({}, Element) -> init_leaf(Element);
 
-insertBT({{Parent, _Height}, {}, RightChild}, Element) when Element < Parent ->
-  NewLeftChild = initLeaf(Element),
-  NewParentHeight = getMaxHeight(NewLeftChild, RightChild) + 1,
+insert_node({{Parent, _Height}, {}, RightChild}, Element) when Element < Parent ->
+  NewLeftChild = init_leaf(Element),
+  NewParentHeight = max_height(NewLeftChild, RightChild) + 1,
   {{Parent, NewParentHeight}, NewLeftChild, RightChild};
 
-insertBT({{Parent, _Height}, LeftChild, {}}, Element) when Element > Parent ->
-  NewRightChild = initLeaf(Element),
-  NewParentHeight = getMaxHeight(LeftChild, NewRightChild) + 1,
+insert_node({{Parent, _Height}, LeftChild, {}}, Element) when Element > Parent ->
+  NewRightChild = init_leaf(Element),
+  NewParentHeight = max_height(LeftChild, NewRightChild) + 1,
   {{Parent, NewParentHeight}, LeftChild, NewRightChild};
 
-insertBT({{Parent, Height}, LeftChild, RightChild}, Element) when Element == Parent ->
+insert_node({{Parent, Height}, LeftChild, RightChild}, Element) when Element == Parent ->
   {{Parent, Height}, LeftChild, RightChild};
 
-insertBT({{Parent, _Height}, LeftChild, RightChild}, Element) ->
+insert_node({{Parent, _Height}, LeftChild, RightChild}, Element) ->
   if
     Element < Parent ->
-      NewLeftChild = insertBT(LeftChild, Element),
-      NewParentHeight = getMaxHeight(NewLeftChild, RightChild) + 1,
+      NewLeftChild = insert_node(LeftChild, Element),
+      NewParentHeight = max_height(NewLeftChild, RightChild) + 1,
       {{Parent, NewParentHeight}, NewLeftChild, RightChild};
     Element > Parent ->
-      NewRightChild = insertBT(RightChild, Element),
-      NewParentHeight = getMaxHeight(LeftChild, NewRightChild) + 1,
+      NewRightChild = insert_node(RightChild, Element),
+      NewParentHeight = max_height(LeftChild, NewRightChild) + 1,
       {{Parent, NewParentHeight}, LeftChild, NewRightChild}
   end.
 
 
-% isEmptyBT: btree → bool
-% isEmptyBT(<BTree>)
+% is_empty: btree → bool
+% is_empty(<BTree>)
 
 
-% equalBT: btree × btree → bool
-% equalBT(<BTree>,<BTree>)
+% equal: btree × btree → bool
+% equal(<BTree>,<BTree>)
 
 
 % Helper
-% initLeaf: elem → btree
-initLeaf(Element) -> {{Element, 1}, {}, {}}.
+% init_leaf: elem → btree
+init_leaf(Element) -> {{Element, 1}, {}, {}}.
 
 
-% getHeight: btree → integer
+% height: btree → integer
 % Get height of parent node of given (partial) tree
-getHeight({}) -> 0;
-getHeight({{_Parent, Height}, _LeftChild, _RightChild}) -> Height.
+height({}) -> 0;
+height({{_Parent, Height}, _LeftChild, _RightChild}) -> Height.
 
 
-% getMaxHeight: btree × btree → integer
+% max_height: btree × btree → integer
 % Get max. height of the two given (partial) trees
-getMaxHeight({}, {}) -> 0;
-getMaxHeight(FirstTree, SecondTree) -> max(getHeight(FirstTree), getHeight(SecondTree)).
+max_height({}, {}) -> 0;
+max_height(FirstTree, SecondTree) -> max(height(FirstTree), height(SecondTree)).
